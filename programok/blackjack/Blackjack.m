6273e03a393fbc5b21e186fb12596036f02124b3
Róbert Nagy <rob.iskola1@gmail.com>
2022-03-09 23:46:06 +0100

    plot

--- programok/blackjack/Blackjack.m
+++ programok/blackjack/Blackjack.m
@@ -25,29 +25,37 @@ function Blackjack(number_of_players, number_of_decks)
         players(i).bet = 0;
     end
 
+    % adatgyujteshez
+    data = struct([]);
+    for i = 1 : number_of_players
+        data(i).name = names(i);
+        data(i).chipsPerRound = [];
+        data(i).hanValuePerRound = [];
+    end
+    averageChipsPerRound = [];
     
     deck = getCards(number_of_decks);
     deck = deck(randperm(length(deck)));
     bet_amounts = [5, 25, 50, 100, 500];
     number_of_players_still_in_game = number_of_players;
     % main loop
+    round_counter = 0;
     while number_of_players_still_in_game > 0
-        % kártyák elokeszitese
-        
-    
+        round_counter = round_counter + 1;
+        % kártyák elokeszitese  
         dealerHand = getCardsFromDeck(2);
         
         for i = 1 : number_of_players
             if players(i).active == true
                 % minden jatekos fogad
                 biggest_bet_value = getBiggestPossibleBet(bet_amounts, players(i).chips);
-            
+                
                 if biggest_bet_value > 0
                     b = randsample(bet_amounts(1:biggest_bet_value), 1);
                     players(i).bet = b;
                     players(i).chips = players(i).chips - b;
                 end
-                fprintf('%c fogadott %d maradt neki %d\n', players(i).name, players(i).bet, players(i).chips);
+                % fprintf('%c fogadott %d maradt neki %d\n', players(i).name, players(i).bet, players(i).chips);
 
                 % minden játékos kap két lapot
                 players(i).hand = getCardsFromDeck(2);
@@ -66,12 +74,15 @@ function Blackjack(number_of_players, number_of_decks)
                     % players(i).hand'
                 end
             end
+            data(i).chipsPerRound(round_counter) = players(i).chips;
+            
             % if players(i).active == true
             %     fprintf('%c ertek: %d keze:\n', players(i).name, getHandValue(players(i).hand));
             %     players(i).hand'
             % end
         end
 
+        
         % ha minden jatekos keszen all, akkor a dealer huz uj kartyakat, ameddig a keze 17 nem lesz
         while getHandValue(dealerHand) < 17
             dealerHand(length(dealerHand) + 1) = getCardsFromDeck(1);
@@ -114,13 +125,32 @@ function Blackjack(number_of_players, number_of_decks)
                 players(i).active = true;
             elseif players(i).chips < bet_amounts(1) && players(i).active == true
                 number_of_players_still_in_game = number_of_players_still_in_game - 1;
+                fprintf('Kiesett: %c %d\n', players(i).name, players(i).chips);
                 players(i).hand = [];
                 players(i).active = false;
+                
             end
             players(i).alreadyPaid = false;
         end
     end
 
+    for k = 1 : round_counter
+        s = 0;
+       for i = 1 : number_of_players
+            s = s + data(i).chipsPerRound(k);
+       end
+       averageChipsPerRound(k) = s / number_of_players;
+    end
+    
+    % plot
+    averageChipsAllGame = mean([averageChipsPerRound]);
+    for i = 1 : number_of_players
+        hold on;
+        plot(data(i).chipsPerRound)
+    end 
+    plot(averageChipsPerRound)
+    line([0 round_counter], [averageChipsAllGame averageChipsAllGame])
+    
     % fprintf('Jatek vege: Elemzes:\n');
     % for i = 1 : number_of_players
     %     fprintf('nev: %c maradek penz: %d\n', players(i).name, players(i).chips);
